68bca07a5b143b85212993324235da2e018b60ee
wingsvn <wing-san.lee@live.nl>
2024-09-20 01:48:09 +0200

    V1 visitekaartje

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <script>
 	export let data;
+	import { draw } from 'svelte/transition';
 </script>
 
-<h1>{data.person.name}</h1>
-<p>{data.person.description}</p>
+<main>
+	<article>
+		<img src="/images/background-image.jpg" alt="background"/>
+		<h1>{data.person.name} {data.person.surname}</h1>
+	</article>
+</main>
+
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	article {
+		position: relative;
+		width: 20em;
+		height: auto;
+		/* text-align: center; */
+		background-color: black;
+	}
+
+	img {
+		width: 100%;
+		height: auto;
+		/* border: 2em solid black; */
+	}
+
+	h1 {
+		/* text-align: center; */
+		position: absolute;
+		top: 20%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		color: white;
+		font-family: 'Fascinate Inline';
+		font-size: 2em;
+		text-align: center;
+	}
+
+</style>
